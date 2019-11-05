9e8ffdbc18345b30e71a7cd6b6295a9ffe06f519
Erick <erick.hx22@gmail.com>
2019-11-05 15:34:40 -0500

    (erick#2) frontend

--- frontend/frontend/src/Components/Products.js
+++ frontend/frontend/src/Components/Products.js
@@ -1,72 +1,44 @@
 import React, { Component } from 'react';
+import { products } from './products.json'
+import CreateProduct from './CreateProduct.js';
 
-import CreateProduct from './CreateProduct'
 
 
 class Products extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            act: 0,
-            index: '',
-            datas: []
+            products
         }
+        this.addProduct = this.addProduct.bind(this)
     }
-    componentDidMount() {
-        this.refs.name.focus()
-    }
-    fSubmit = (e) => {
-        e.preventDefault()
-        console.log('try')
-
-        let datas = this.state.datas
-        let name = this.refs.name.value
-        let price = this.refs.price.value
-        let discount = this.refs.discount.value
-        if (this.state.act === 0) {
-            let data = {
-                name, price, discount
-            }
-            datas.push(data)
-        } else {
-            let index = this.state.index
-            datas[index].name = name
-            datas[index].price = price
-            datas[index].discount = discount
-        }
-
-
+    addProduct(product) {
         this.setState({
-            datas: datas,
-            act: 0
+            products: [...this.state.products, product]
         })
-        this.refs.myForm.reset()
-        this.refs.name.focus()
     }
-    fRemove = (i) => {
-        let datas = this.state.datas
-        datas.splice(i, 1)
+    removeProduct(index) {
         this.setState({
-            datas: datas
+            products: this.state.products.filter((e, i) => {
+                return i !== index
+            })
         })
-        this.refs.myForm.reset()
-        this.refs.name.focus()
     }
-    fEdit = (i) => {
-        let data = this.state.datas[i]
-        this.refs.name.value = data.name
-        this.refs.price.value = data.price
-        this.refs.discount.value = data.discount
-        this.setState({
-            act: 1,
-            index: i
-        })
-        this.refs.name.focus()
-
-    }
-
     render() {
-        let datas = this.state.datas
+        let products = this.state.products.map((product, i) => {
+            return (
+                <li className="single-product" key={i}>
+                    <div className="discount">{product.discount}</div>
+                    <div className="img"></div>
+                    <div className="inner">
+                        <h5>{product.name} N°{i + 1}</h5>
+                        <p>{product.price}</p>
+                        <button className="btn">VIEW DETAILS</button>
+                        <button className="btn" onClick={this.removeProduct.bind(this, i)}>Delete</button>
+                    </div>
+                </li>
+            )
+        })
         return (
             <section className="home products">
                 <div className="section-company">
@@ -92,45 +64,15 @@ class Products extends Component {
                     </div>
 
 
-                    <form ref="myForm" className="myForm" >
-                        <input type="text" ref="name" placeholder="Name Product" />
-                        <input type="text" ref="price" placeholder="Price " />
-                        <input type="text" ref="discount" placeholder="Discount " />
-                        <button onClick={this.fSubmit} className="btn" >Create Now</button>
-                    </form>
+                    <CreateProduct onAddProduct={this.addProduct} />
 
 
                     <ul className="container-product">
-                        <li className="single-product">
-                            <div className="discount">-20%</div>
-                            <div className="img"></div>
-                            <div className="inner">
-                                <h5>Product N°1</h5>
-                                <p>$200.00</p>
-                                <button className="btn">VIEW DETAILS</button>
-                            </div>
-                        </li>
-                        {datas.map((data, i) =>
-                            <li key={i} className="single-product">
-
-                                <div className="discount">{data.discount}</div>
-                                <div className="img"></div>
-                                <div className="inner">
-                                    <h5>{data.name} N°{i + 2}</h5>
-                                    <p>{data.price}</p>
-                                    <button onClick={() => this.fRemove(i)} className="btn">Remove</button>
-                                    <button onClick={() => this.fEdit(i)} className="btn">Edit</button>
-                                </div>
-
-                            </li>
-                        )}
+                        {products}
 
                     </ul>
 
                 </div>
-                <div className="footer"></div>
-
-
             </section>
 
         )
